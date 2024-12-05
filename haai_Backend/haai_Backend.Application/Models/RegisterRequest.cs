6eb1be2b7a59b38528a9299530259c751cbce2ad
Madhubhashana Mahindarathne <dinushamadhubhashana1999@gmail.com>
2024-12-05 15:45:04 +0530

    Create LoginRequest and Register

---
+++ haai_Backend/haai_Backend.Application/Models/RegisterRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace haai_Backend.Application.Models
+{
+    public class RegisterRequest
+    {
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+        public required string role { get; set; }
+    }
+}
--- haai_Backend/haai_Backend.Infrastructure/Data/AppDbContext.cs
+++ haai_Backend/haai_Backend.Infrastructure/Data/AppDbContext.cs
@@ -6,8 +6,11 @@ using System.Threading.Tasks;
 
 namespace haai_Backend.Infrastructure.Data
 {
-    public class AppDbContext
+    public class AppDbContext: DbContext
     {
+        public AppDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
 
+        }
     }
 }

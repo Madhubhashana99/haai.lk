b0aba6a0291eaea223e11cc3197f1fe194d470e9
Theekshana <shehaltheekshana@gmail.com>
2024-12-05 15:01:34 +0530

    create user,categories,product Domain files

---
+++ haai_Backend/haai_Backend.Domain/catergories.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace haai_Backend.Domain
+{
+    [Table("categories")]
+    public class Category
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [MaxLength(255)]
+        public string Description { get; set; }
+
+        public int? ParentCategoryId { get; set; }
+
+        [ForeignKey(nameof(ParentCategoryId))]
+        public Category ParentCategory { get; set; }
+
+        public ICollection<Category> SubCategories { get; set; } = new List<Category>();
+
+        public bool IsActive { get; set; } = true;
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+    }
+}
---
+++ haai_Backend/haai_Backend.Domain/products.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema; // Added for MaxLengthAttribute
+
+
+namespace haai_Backend.Domain
+{
+    [Table ("products")]
+   public class Product
+    {
+        public Guid id { get; set; }
+
+        [MaxLength (50)]
+        public string name { get; set; }
+
+        [MaxLength (50)]
+        public string description { get; set; }
+
+        [MaxLength (50)]
+        public string categoryId { get; set; }
+
+        [MaxLength(50)]
+        public string price { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+
+
+    }
+}

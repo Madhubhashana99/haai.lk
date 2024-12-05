287be793f88710d0f75f6c2065a49bf41de28559
Theekshana <shehaltheekshana@gmail.com>
2024-12-05 15:13:52 +0530

    create orders Domain file

---
+++ haai_Backend/haai_Backend.Domain/orders.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace haai_Backend.Domain
+{
+    [Table("orders")]
+    public class Order
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public Guid UserId { get; set; }
+
+        [Required]
+        public decimal TotalPrice { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [Required]
+        [MaxLength(50)] // Restrict status to a maximum of 50 characters
+        public string Status { get; set; }
+
+        [Required]
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        [Required]
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
